9833f5e37089fdf27e339af69166e5f51f603fe1
loganODI <cmldod@gmail.com>
2017-11-21 12:12:34 +0200

    Added GPS and Json file, as well as google button

--- app/src/main/java/com/gpslpm/com/gpstest/MainActivity.java
+++ app/src/main/java/com/gpslpm/com/gpstest/MainActivity.java
@@ -22,10 +22,10 @@ package com.gpslpm.com.gpstest;
  * Activity to demonstrate basic retrieval of the Google user's ID, email address, and basic
  * profile.
  */
-public class SignInActivity extends AppCompatActivity implements
+public class MainActivity extends AppCompatActivity implements
         View.OnClickListener {
 
-    private static final String TAG = "SignInActivity";
+    private static final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 9001;
 
     private GoogleSignInClient mGoogleSignInClient;
